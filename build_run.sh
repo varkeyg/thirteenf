c1b801349e9af4a3e7d110beda2c30b0cf3b2748
Varkey, Geo <geo.varkey@gmail.com>
2022-12-29 09:53:47 -0800

    by Geo

--- build_run.sh
+++ build_run.sh
@@ -5,19 +5,19 @@ sudo chmod g+rwx "$HOME/.docker" -R
 
 
 # Build the image. Make sure to run in the directory where the Dockerfile file is available
-docker build -t dev_img .
+sudo docker build -t dev_img .
 
 # Remove the container if it already exists
-docker rm /dev_container
+sudo docker rm /dev_container
 
 # Create a volume for data on the parent host
-docker volume create data
+sudo docker volume create data
 
 # create volume for source code on the parent host
-docker volume create workspace
+sudo docker volume create workspace
 
 
 # Run the container & Mount the volumes
-docker run --name dev_container \
+sudo docker run --name dev_container \
 --mount source=workspace,target=/home/apache/workspace \
 --mount source=data,target=/home/apache/data -it dev_img
